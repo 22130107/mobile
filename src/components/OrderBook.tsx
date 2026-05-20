3c14febb0bf435152936bf7d0b8916f53cb99ff9
22130107 <22130107@st.hcmuaf.edu.vn>
2026-05-20 19:17:36 +0700

    1

--- src/components/OrderBook.tsx
+++ src/components/OrderBook.tsx
@@ -364,7 +364,7 @@ export default function OrderBook({ onNavigate }: OrderBookProps) {
                     className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm"
                     defaultValue={selectedOrder.price}
                     type="number"
-                    step="0.1"
+                    step="any"
                     required
                   />
                 </div>
@@ -386,7 +386,7 @@ export default function OrderBook({ onNavigate }: OrderBookProps) {
                     className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm"
                     defaultValue={selectedOrder.matched_price || 0}
                     type="number"
-                    step="0.01"
+                    step="any"
                   />
                 </div>
 
